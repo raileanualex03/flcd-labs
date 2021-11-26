6a0a70e1713b65666e3b9d7d74ff0a899666d9c3
Alex Raileanu <62054366+raileanualex03@users.noreply.github.com>
2021-11-26 12:53:03 +0200

    Finish lab5

--- main.py
+++ main.py
@@ -1,107 +1,103 @@
-class FiniteAutomaton:
-    def __init__(self, fileName):
-        self.states = list()
-        self.alphabet = list()
-        self.transitions = dict()
-        self.initialState = None
-        self.finalStates = list()
-        self.neighbours = dict(list())
-        self.readFromFile(fileName)
+class Grammar:
+    def __init__(self, file_name):
+        self.non_terminal_symbols = list()
+        self.terminal_symbols = list()
+        self.productions = dict(list())
+        self.start_symbol = None
+        self.read_from_file(file_name)
 
-    def readFromFile(self, fileName):
-        file = open(fileName, "r")
+    def read_from_file(self, file_name):
+        file = open(file_name, "r")
         lines = file.readlines()
 
-        self.states = lines[0].strip().split(',')
+        self.non_terminal_symbols = lines[0].strip().split(',')
+        self.terminal_symbols = lines[1].strip().split(',')
+        self.start_symbol = lines[2]
 
-        for state in self.states:
-            self.neighbours[state] = []
+        for index in range(3, len(lines)):
+            elements = lines[index].strip().split('->')
+            non_terminal_symbol = elements[0].strip()
+            productions_for_symbol = elements[1].split('|')
+            self.productions[non_terminal_symbol] = productions_for_symbol
 
-        self.alphabet = lines[1].strip().split(',')
+    def check_if_cfg(self):
+        # we will go through all the productions and check if the terminal node is on the left side
+        for symbol in self.productions:
+            productions = self.productions[symbol]
+            for production in productions:
+                if len(production) >= 1:
+                    if production[0] in self.terminal_symbols and production[1] in self.terminal_symbols:
+                        return False
+        return True
 
-        self.initialState = lines[2].strip()
-
-        self.finalStates = lines[3].strip().split(',')
-
-        for index in range(4, len(lines)):
-            elements = lines[index].strip().split(',')
-            first = elements[0]
-            second = elements[1]
-            values = elements[2:]
-            self.neighbours[first].append(second)
-            self.transitions[(first, second)] = values
-
-        file.close()
-
-    def checkIfSequenceAccepted(self, sequence, currentState):
-        if len(sequence) == 0:
-            return currentState in self.finalStates
-
-        for neighbor in self.neighbours[currentState]:
-            values = self.transitions[(currentState, neighbor)]
-            if sequence[0] in values:
-                if self.checkIfSequenceAccepted(sequence[1:], neighbor):
-                    return True
-
-        return False
-
-    def checkSequence(self, sequence):
-        return self.checkIfSequenceAccepted(sequence, self.initialState)
-
-    def showMenu(self):
+    def print_menu(self):
         print('Please choose an option: \n'
-              '\t1. show states\n'
-              '\t2. show alphabet\n'
-              '\t3. show transitions\n'
-              '\t4. show initial state\n'
-              '\t5. show final states\n'
-              '\t6. check if sequence is accepted\n'
+              '\t1. show non terminals\n'
+              '\t2. show terminals\n'
+              '\t3. show starting symbol\n'
+              '\t4. show all productions\n'
+              '\t5. show productions for symbol \n'
+              '\t6. check if grammar is context free\n'
               '\tx. quit\n')
 
-    def showState(self):
-        print('The states are: ' + str(self.states))
+    def show_non_terminals(self):
+        print('The non terminals: ' + str(self.non_terminal_symbols))
 
-    def showAlphabet(self):
-        print('The alphabet is: ' + str(self.alphabet))
+    def show_terminals(self):
+        print('The terminals: ' + str(self.terminal_symbols))
 
-    def showTransitions(self):
-        print('The transitions are: ' + str(self.transitions))
+    def show_starting_symbol(self):
+        print('The starting symbol: ' + str(self.start_symbol))
 
-    def showInitialState(self):
-        print('The initial state is: ' + str(self.initialState))
+    def show_productions(self):
+        toPrint = ""
+        print(self.productions)
+        for symbol in self.productions:
+            productions_for_symbol = self.productions[symbol]
+            toPrint += symbol + " -> "
+            for production in productions_for_symbol:
+                toPrint += production + " | "
+            toPrint += "\n\t"
+        print('The productions: \n\t' + toPrint)
+    
+    def show_product_for_symbol(self):
+        symbol = input('The given symbol is: ')
+        toPrint = ""
+        productions_for_symbol = self.productions[symbol]
+        for production in productions_for_symbol:
+            toPrint += symbol + " -> " + production + "\n\t"
+        print('The productions for symbol ' + symbol + ' : \n\t' + toPrint)    
 
-    def showFinalStates(self):
-        print('The final states are: ' + str(self.finalStates))
-
-    def showAcceptance(self):
-    	sequence = input('The sequence to be checked: ')
-    	isAccepted = self.checkSequence(sequence)
-    	action = ' is ' if isAccepted else ' is not '
-    	result = 'The sequence ' + sequence + action + 'accepted by the current FA\n'
-    	print(result)
+    def show_cfg(self):
+        result = self.check_if_cfg()
+        isCFG = "is" if result else "is not"
+        print('The given grammar ' + isCFG + ' a cfg')
 
     def menu(self):
-    	options = {
-    		'1': self.showState,
-    		'2': self.showAlphabet,
-    		'3': self.showTransitions,
-    		'4': self.showInitialState,
-    		'5': self.showFinalStates,
-    		'6': self.showAcceptance
-    	}
-    	while True:
-            self.showMenu()
+        options = {
+            1: self.show_non_terminals,
+            2: self.show_terminals,
+            3: self.show_starting_symbol,
+            4: self.show_productions,
+            5: self.show_product_for_symbol,
+            6: self.show_cfg
+        }
+
+        isDone = False
+        print('Welcome to Lab5!\n')
+        while True:
+            self.print_menu()
             option = input('Your choice is: ')
+
             if option == 'x':
                 print('Thanks for using this!')
-                return
+                quit()
             try:
-            	options[option]()
+                options[int(option)]()
             except KeyError as e:
-            	print('ERROR: Please add a valid choice!')
+                print("Please provide valid key! " + str(e) + " not found!" )            
 
 
 if __name__ == '__main__':
-    fa = FiniteAutomaton('fa.in')
-    fa.menu()
-
+    grammar = Grammar('g1.txt')
+    grammar.menu()
